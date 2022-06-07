cc1cca15b21d6e4938adb4bcde96c4a4ad95bf93
Bruce Szudera Wienand <bruce@ternary.app>
2022-06-07 18:13:28 -0400

    Format query_test

--- query_test.go
+++ query_test.go
@@ -14,10 +14,25 @@ func Test_DateRangeMarshalJSON(t *testing.T) {
 		dateRange cube.DateRange
 		expected  *string
 	}{
-		{cube.DateRange{RelativeRange: boxString("two weeks ago")}, boxString(`"two weeks ago"`)},
-		{cube.DateRange{AbsoluteRange: []string{"2021-04-20", "2021-04-21"}}, boxString(`["2021-04-20","2021-04-21"]`)},
-		{cube.DateRange{RelativeRange: boxString("two weeks ago"), AbsoluteRange: []string{"2021-04-20", "2021-04-21"}}, nil},
-		{cube.DateRange{}, nil},
+		{
+			cube.DateRange{RelativeRange: boxString("two weeks ago")},
+			boxString(`"two weeks ago"`),
+		},
+		{
+			cube.DateRange{AbsoluteRange: []string{"2021-04-20", "2021-04-21"}},
+			boxString(`["2021-04-20","2021-04-21"]`),
+		},
+		{
+			cube.DateRange{
+				RelativeRange: boxString("two weeks ago"),
+				AbsoluteRange: []string{"2021-04-20", "2021-04-21"},
+			},
+			nil,
+		},
+		{
+			cube.DateRange{},
+			nil,
+		},
 	}
 
 	for _, tcase := range battery {
